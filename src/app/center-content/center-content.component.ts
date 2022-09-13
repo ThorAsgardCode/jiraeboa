b628807f9cd0a7231226999f1d6d561a3a1690ff
ThorAsgardCode <88061390+ThorAsgardCode@users.noreply.github.com>
2022-09-13 16:59:19 +0100

    7 files modified

--- src/app/center-content/center-content.component.ts
+++ src/app/center-content/center-content.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import 'fontawesome';
 @Component({
   selector: 'app-center-content',
   templateUrl: './center-content.component.html',
-  styleUrls: ['./center-content.component.css']
+  styleUrls: ['./center-content.component.css'],
 })
 export class CenterContentComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import 'fontawesome';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
